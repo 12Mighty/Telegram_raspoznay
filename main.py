461e427ac7bbaf732aefc68299598e9cd62b482b
12Mighty <122643954+12Mighty@users.noreply.github.com>
2023-10-26 22:12:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 from PIL import Image
 
 # Токен бота
-API_TOKEN = 'ВАШ ТЕЛЕГРАМ ТОКЕН БОТА'
+API_TOKEN = 'ВАШ ТОКЕН'
 
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
@@ -28,23 +28,27 @@ async def on_start(message: types.Message):
 # Обработчик изображений
 @dp.message_handler(content_types=types.ContentType.PHOTO)
 async def process_photo(message: types.Message):
-    # Загрузка фотографии
-    file_id = message.photo[-1].file_id
-    file = await bot.download_file_by_id(file_id)
-    file_bytes = file.read()
-    photo = io.BytesIO(file_bytes)
-    img = Image.open(photo)
+    try:
+        # Загрузка фотографии
+        file_id = message.photo[-1].file_id
+        file = await bot.download_file_by_id(file_id)
+        file_bytes = file.read()
+        photo = io.BytesIO(file_bytes)
+        img = Image.open(photo)
 
-    # Распознавание текста
-    result = reader.readtext(np.array(img))
+        # Распознавание текста
+        result = reader.readtext(np.array(img))
 
-    # Подготовка ответа
-    recognized_text = '\n'.join([entry[1] for entry in result])
+        # Подготовка ответа
+        recognized_text = '\n'.join([entry[1] for entry in result])
 
-    if recognized_text:
-        await message.answer(f"Распознанный текст на изображении:\n{recognized_text}", parse_mode=ParseMode.MARKDOWN)
-    else:
-        await message.answer("Текст не найден на изображении.")
+        if recognized_text:
+            await message.answer(f"Распознанный текст на изображении:\n{recognized_text}", parse_mode=ParseMode.MARKDOWN)
+        else:
+            await message.answer("Текст не найден на изображении.")
+    except Exception as e:
+        # Логирование ошибки
+        logging.error(f"Error during text recognition: {str(e)}")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
